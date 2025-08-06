cfed0c0648f9f735cf6f850d825a864a65ff1064
TAPAN KUIRI <130714228+TapanKuiri@users.noreply.github.com>
2025-08-07 02:50:09 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import { Hero } from '../components/hero'
+ 
 import { LatestCollection } from '../components/LatestCollection'
 import { BestSeller } from '../components/BestSeller'
 import { OurPolicy } from '../components/footer/OurPolicy'
-import { NewsletterBox } from '../components/NewsletterBox'
+ 
 
 export const Home = () => {
   return (
-    <div> 
-      {/* <Hero/>  */}
+    <div className=' md:mx-10 mx-2'> 
+      
       <LatestCollection/>
       <BestSeller/>
       <OurPolicy/>
-      {/* <NewsletterBox/> */}
+       
     </div>
   )
 }
+
